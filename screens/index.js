047864c8b8bde4f592ac615778072c9d49c218d9
k-hsu <khsuzmail@gmail.com>
2021-12-05 23:06:03 -0600

    Merge branch '8-add-jest-testing' of https://github.com/Brian-Go-Developer/mow into 8-add-jest-testing

--- screens/index.js
+++ screens/index.js
@@ -2,7 +2,4 @@ export { default as Home } from "./home/home.js";
 export { default as Profile } from "./profile/profile.js";
 export { default as Explore } from "./explore/explore.js";
 export { default as Onboarding } from "./onboarding/onboarding.js";
-<<<<<<< HEAD
 export { default as EnableLocation } from "./enable-location.js";
-=======
->>>>>>> b9e8e2ea18bc43011abe8ebef542a169059a351a
